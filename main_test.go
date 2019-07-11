39304dca14378af49164c5a1d45e323422df6b3d
Charles Comstock <dgtized@gmail.com>
2019-07-10 23:14:44 -0500

    Simplify yet again

--- main_test.go
+++ main_test.go
@@ -5,37 +5,34 @@ import (
 	"gotest.tools/assert"
 )
 
+func runstack(t *testing.T, before []string, after []string) {
+	assert.DeepEqual(t, consume(before), after)
+}
+
 func TestAdd(t *testing.T) {
-	output := consume([]string {"5", "6", "+"})
-	assert.DeepEqual(t, output, []string {"11"})
+	runstack(t, []string {"5", "6", "+"}, []string {"11"})
 }
 
 func TestSub(t *testing.T) {
-	output := consume([]string {"7", "3", "-"})
-	assert.DeepEqual(t, output, []string {"4"})
+	runstack(t, []string {"7", "3", "-"}, []string {"4"})
 }
 
 func TestDup(t *testing.T) {
-	output := consume([]string {"5", "dup"})
-	assert.DeepEqual(t, output, []string {"5", "5"})
+	runstack(t, []string {"5", "dup"}, []string {"5", "5"})
 }
 
 func TestSwap(t *testing.T) {
-	output := consume([]string {"5", "6", "swap"})
-	assert.DeepEqual(t, output, []string {"6", "5"})
+	runstack(t, []string {"5", "6", "swap"}, []string {"6", "5"})
 }
 
 func TestOver(t *testing.T) {
-	output := consume([]string {"5", "6", "over"})
-	assert.DeepEqual(t, output, []string {"5", "6", "5"})
+	runstack(t, []string {"5", "6", "over"}, []string {"5", "6", "5"})
 }
 
 func TestRot(t *testing.T) {
-	output := consume([]string {"5", "6", "7", "rot"})
-	assert.DeepEqual(t, output, []string {"6", "7", "5"})
+	runstack(t, []string {"5", "6", "7", "rot"}, []string {"6", "7", "5"})
 }
 
 func TestMultiple(t *testing.T) {
-	output := consume([]string {"5", "6", "+", "dup", "-"})
-	assert.DeepEqual(t, output, []string {"0"})
+	runstack(t, []string {"5", "6", "+", "dup", "-"}, []string {"0"})
 }
